d12b5a9383b8e85375829dacefbf236d0ddb1ed8
Jacob Alperin-Sheriff <jacobalperin-sheriff@jacob-alperin-sheriffs-macbook-pro.local>
2017-08-01 22:20:53 -0400

    stuff

--- kartograph/maplayer.py
+++ kartograph/maplayer.py
@@ -31,7 +31,7 @@ class MapLayer(object):
         self.features=[]
         self.special_fips = special_fips
         self.max_area_for_circle=.001
-        self.high_exp_factor=1.75
+        self.high_exp_factor=2.50
         if cache is not None and 'features' in cache:
             self.proj_feat_cache=cache['features']
         elif cache is not None:
